deed31a51c53caea3a21e6229481c30180b5d5e7
Ulises Vargas <ext_uvargasds@falabella.cl>
2023-10-16 18:39:52 -0400

    fix/file-downloads-by-user-request-query-params

--- routes/downloads.mjs
+++ routes/downloads.mjs
@@ -2,8 +2,8 @@ import express from "express";
 import { errorHandler } from "../utils/error_handling/errorHandler.mjs";
 const downloads = express.Router();
 
-downloads.get("/txt", (request, response, next) => {
-  const file = "./assets/txt/test.txt";
+downloads.get("/txt/:file", (request, response, next) => {
+  const file = `./assets/txt/${request.params.file}`;
   response.download(file, (error) => {
     if (error) {
       errorHandler.notFound(request, response, next);
